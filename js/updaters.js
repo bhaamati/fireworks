0dafd2a167178fed67fa1df4ffdbad1b8fea0181
dchege711 <d.chege711@gmail.com>
2019-05-12 14:56:53 -0400

    Set up skeleton for the BasicFireworksUpdater

--- js/updaters.js
+++ js/updaters.js
@@ -306,4 +306,141 @@ ClothUpdater.prototype.update = function ( particleAttributes, alive, delta_t, w
     particleAttributes.velocity.needsUpdate = true;
     particleAttributes.lifetime.needsUpdate = true;
     particleAttributes.size.needsUpdate = true;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Euler updater
+////////////////////////////////////////////////////////////////////////////////
+
+function EulerUpdater ( opts ) {
+    this._opts = opts;
+    return this;
+};
+
+
+EulerUpdater.prototype.updatePositions = function ( particleAttributes, alive, delta_t ) {
+    var positions  = particleAttributes.position;
+    var velocities = particleAttributes.velocity;
+
+    for ( var i  = 0 ; i < alive.length ; ++i ) {
+        if ( !alive[i] ) continue;
+        var p = getElement( i, positions );
+        var v = getElement( i, velocities );
+        p.add( v.clone().multiplyScalar( delta_t ) );
+        setElement( i, positions, p );
+    }
+};
+
+EulerUpdater.prototype.updateVelocities = function ( particleAttributes, alive, delta_t ) {
+    var positions = particleAttributes.position;
+    var velocities = particleAttributes.velocity;
+    var gravity = this._opts.externalForces.gravity;
+    var attractors = this._opts.externalForces.attractors;
+    var dampenings = particleAttributes.dampening;
+
+    for ( var i = 0 ; i < alive.length ; ++i ) {
+        if ( !alive[i] ) continue;
+        // ----------- STUDENT CODE BEGIN ------------
+        var p = getElement( i, positions );
+        var v = getElement( i, velocities );
+        // now update velocity based on forces...
+
+        setElement( i, velocities, v );
+        // ----------- STUDENT CODE END ------------
+    }
+
+};
+
+EulerUpdater.prototype.updateColors = function ( particleAttributes, alive, delta_t ) {
+    var colors    = particleAttributes.color;
+
+    for ( var i = 0 ; i < alive.length ; ++i ) {
+
+        if ( !alive[i] ) continue;
+        // ----------- STUDENT CODE BEGIN ------------
+        var c = getElement( i, colors );
+
+        setElement( i, colors, c );
+        // ----------- STUDENT CODE END ------------
+    }
+};
+
+EulerUpdater.prototype.updateSizes= function ( particleAttributes, alive, delta_t ) {
+    var sizes    = particleAttributes.size;
+
+    for ( var i = 0 ; i < alive.length ; ++i ) {
+
+        if ( !alive[i] ) continue;
+        // ----------- STUDENT CODE BEGIN ------------
+        var s = getElement( i, sizes );
+
+        setElement( i, sizes, s );
+        // ----------- STUDENT CODE END ------------
+    }
+
+};
+
+EulerUpdater.prototype.updateLifetimes = function ( particleAttributes, alive, delta_t) {
+    var positions     = particleAttributes.position;
+    var lifetimes     = particleAttributes.lifetime;
+
+    for ( var i = 0 ; i < alive.length ; ++i ) {
+
+        if ( !alive[i] ) continue;
+
+        var lifetime = getElement( i, lifetimes );
+
+        if ( lifetime < 0 ) {
+            killPartilce( i, particleAttributes, alive );
+        } else {
+            setElement( i, lifetimes, lifetime - delta_t );
+        }
+    }
+
+};
+
+EulerUpdater.prototype.collisions = function ( particleAttributes, alive, delta_t ) {
+    if ( !this._opts.collidables ) {
+        return;
+    }
+    if ( this._opts.collidables.bouncePlanes ) {
+        for (var i = 0 ; i < this._opts.collidables.bouncePlanes.length ; ++i ) {
+            var plane = this._opts.collidables.bouncePlanes[i].plane;
+            var damping = this._opts.collidables.bouncePlanes[i].damping;
+            Collisions.BouncePlane( particleAttributes, alive, delta_t, plane, damping );
+        }
+    }
+
+    if ( this._opts.collidables.sinkPlanes ) {
+        for (var i = 0 ; i < this._opts.collidables.sinkPlanes.length ; ++i ) {
+            var plane = this._opts.collidables.sinkPlanes[i].plane;
+            Collisions.SinkPlane( particleAttributes, alive, delta_t, plane );
+        }
+    }
+
+    if ( this._opts.collidables.spheres ) {
+        for (var i = 0 ; i < this._opts.collidables.spheres.length ; ++i ) {
+            Collisions.Sphere( particleAttributes, alive, delta_t, this._opts.collidables.spheres[i] );
+        }
+    }
+};
+
+EulerUpdater.prototype.update = function ( particleAttributes, alive, delta_t ) {
+
+    this.updateLifetimes( particleAttributes, alive, delta_t );
+    this.updateVelocities( particleAttributes, alive, delta_t );
+    this.updatePositions( particleAttributes, alive, delta_t );
+
+    this.collisions( particleAttributes, alive, delta_t );
+
+    this.updateColors( particleAttributes, alive, delta_t );
+    this.updateSizes( particleAttributes, alive, delta_t );
+
+    // tell webGL these were updated
+    particleAttributes.position.needsUpdate = true;
+    particleAttributes.color.needsUpdate = true;
+    particleAttributes.velocity.needsUpdate = true;
+    particleAttributes.lifetime.needsUpdate = true;
+    particleAttributes.size.needsUpdate = true;
+
 }
